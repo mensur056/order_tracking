e73c764a773c3adc329a2b6885962a97b205daf3
mensur056 <mansur.sarxanov@gmail.com>
2023-05-14 22:51:38 +0400

    test

--- order_app/lib/app.dart
+++ order_app/lib/app.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:order_app/core/bloc/sign_in/sign_in_cubit.dart';
 
+import 'core/data/contractors/i_auth_repository.dart';
+import 'core/data/repository/impl_auth_repository.dart';
 import 'feature/screens/sign_in/sign_in_page.dart';
 
 class MyApp extends StatelessWidget {
@@ -7,10 +11,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Order Tracking',
-      home: SignInPage(),
+    return RepositoryProvider<IAuthRepository>(create: (context) => ImplAuthRepository(),
+      child: MultiBlocProvider(providers: [ BlocProvider<SignInCubit>(
+        create: (context) => SignInCubit(context.read<IAuthRepository>()),
+      ),],
+        child: const MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Order Tracking',
+          home: SignInPage(),
+        ),
+      ),
     );
   }
 }
--- order_app/lib/feature/screens/sign_in/sign_in_page.dart
+++ order_app/lib/feature/screens/sign_in/sign_in_page.dart
@@ -1,13 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:kartal/kartal.dart';
+import 'package:order_app/core/bloc/sign_in/sign_in_cubit.dart';
+import 'package:order_app/core/bloc/sign_in/sign_in_state.dart';
+import 'package:order_app/feature/screens/home/home_page.dart';
 
 class SignInPage extends StatelessWidget {
   const SignInPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color(0xFF202124),
-      body: Column(),
+    return Scaffold(
+      backgroundColor: const Color(0xFF202124),
+      body: BlocConsumer<SignInCubit, SignInState>(
+        listener: (context, state) {
+          if (state is SignInFailure) {
+            ScaffoldMessenger.of(context)
+                .showSnackBar(const SnackBar(content: Text("There is a error")));
+          } else if (state is SignInSuccess) {
+            context.navigateToPage(const HomePage());
+          }
+        },
+        listenWhen: (_, current) => current is SignInFailure || current is SignInSuccess,
+        buildWhen: (_, current) => current is! SignInSuccess,
+        builder: (context, state) {
+          return Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  width: 200,
+                  height: 200,
+                  decoration:
+                      BoxDecoration(borderRadius: BorderRadius.circular(100), color: Colors.white),
+                )
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
