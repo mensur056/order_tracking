b6621ed4356ef0fa9b4da99c2f168068d2ad01d4
mensur056 <mansur.sarxanov@gmail.com>
2023-05-21 10:51:24 +0400

    est

--- order_app/lib/feature/screens/home/home_page.dart
+++ order_app/lib/feature/screens/home/home_page.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:kartal/kartal.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Welcome'),
-      ),
-    );
+    return Scaffold(
+        backgroundColor: const Color(0xFF36383D),
+        body: Column(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Container(color: const Color(0xFF36383D)),
+            ),
+            Expanded(
+              flex: 3,
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: context.roundedRectangleBorderMedium.borderRadius),
+              ),
+            ),
+          ],
+        ));
   }
 }
