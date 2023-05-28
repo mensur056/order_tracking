413584889af0c4e1eeca41b3503931ab130758f8
mensur056 <mansur.sarxanov@gmail.com>
2023-05-28 08:33:05 +0400

    tet

--- order_app/lib/feature/screens/home/home_page.dart
+++ order_app/lib/feature/screens/home/home_page.dart
@@ -13,73 +13,89 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.backColor,
-      body: BlocBuilder<HomeCubit, HomeState>(
-        builder: (context, state) {
-          if (state is HomeInProgress) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          } else if (state is HomeSucces) {
-            final projectValue = state.items;
-            return Column(
-              children: [
-                Expanded(
-                  flex: 1,
-                  child: Container(color: AppColors.backColor),
-                ),
-                Expanded(
-                  flex: 3,
-                  child: Container(
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: context.roundedRectangleBorderMedium.borderRadius),
-                    child: Padding(
-                      padding: context.paddingMedium,
-                      child: Column(
-                        children: [
-                          const Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text('Last projects'),
-                              Text('All Projects'),
-                            ],
-                          ),
-                          SizedBox(
-                            height: context.dynamicHeight(0.6),
-                            child: ListView.builder(
-                              itemCount: projectValue?.length,
-                              itemBuilder: (context, index) {
-                                return Padding(
-                                  padding: const EdgeInsets.only(bottom: 16),
-                                  child: Container(
-                                      decoration: BoxDecoration(
-                                          border: Border.all(color: Colors.black),
-                                          borderRadius:
-                                              const BorderRadius.all(Radius.circular(16))),
-                                      child: ListTile(
-                                          trailing: const Icon(Icons.navigate_next_outlined),
-                                          subtitle: Text(projectValue?[index].subtitle ?? ''),
-                                          leading: Image.network(
-                                              projectValue?[index].image.toString() ?? ''),
-                                          title: Text(projectValue?[index].title ?? ''))),
-                                );
-                              },
+      body: _body(),
+    );
+  }
+
+  Widget _body() {
+    return BlocBuilder<HomeCubit, HomeState>(
+      builder: (context, state) {
+        if (state is HomeInProgress) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (state is HomeSucces) {
+          final projectValue = state.items;
+          return Column(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Container(color: AppColors.backColor),
+              ),
+              Expanded(
+                flex: 3,
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: context.roundedRectangleBorderMedium.borderRadius),
+                  child: Padding(
+                    padding: context.paddingMedium,
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Last projects',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodyLarge
+                                  ?.copyWith(fontSize: 20, fontWeight: FontWeight.bold),
                             ),
+                            Text(
+                              'All Projects',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .labelLarge
+                                  ?.copyWith(color: Colors.red),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: context.dynamicHeight(0.6),
+                          child: ListView.builder(
+                            itemCount: projectValue?.length,
+                            itemBuilder: (context, index) {
+                              return Padding(
+                                padding: const EdgeInsets.only(bottom: 16),
+                                child: Container(
+                                    decoration: BoxDecoration(
+                                        border: Border.all(color: Colors.black),
+                                        borderRadius: const BorderRadius.all(Radius.circular(16))),
+                                    child: ListTile(
+                                        onTap: () {},
+                                        trailing: const Icon(Icons.navigate_next_outlined),
+                                        subtitle: Text(projectValue?[index].subtitle ?? ''),
+                                        leading: Image.network(
+                                            projectValue?[index].image.toString() ?? ''),
+                                        title: Text(projectValue?[index].title ?? ''))),
+                              );
+                            },
                           ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
-              ],
-            );
-          } else {
-            return const Center(
-              child: Text("There is a error"),
-            );
-          }
-        },
-      ),
+              ),
+            ],
+          );
+        } else {
+          return const Center(
+            child: Text("There is a error"),
+          );
+        }
+      },
     );
   }
 }
